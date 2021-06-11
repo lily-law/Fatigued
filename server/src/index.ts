5089f9d36f4c291287b145d88a66b52972489df4
lily-law <lilylaw.dev@gmail.com>
2021-06-11 18:36:16 +0100

    Wrap sever startup in async function - Await for db connection before starting up server

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,40 +15,43 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 // Initialise connection (production) or in memory (development) database
-dbConnection.init('fatigued')
+const init = async () => {
+  await dbConnection.init('fatigued')
 
-app.set('port', PORT)
-app.use(json())
-app.use(urlencoded({ extended: false }))
+  app.set('port', PORT)
+  app.use(json())
+  app.use(urlencoded({ extended: false }))
 
-app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000, // Day
-    keys: [process.env.SESSION_COOKIE_KEY || ''],
-  }),
-)
+  app.use(
+    cookieSession({
+      maxAge: 24 * 60 * 60 * 1000, // Day
+      keys: [process.env.SESSION_COOKIE_KEY || ''],
+    }),
+  )
 
-app.use(passport.initialize())
-app.use(passport.session())
+  app.use(passport.initialize())
+  app.use(passport.session())
 
-app.use('/auth', authRouter)
-app.use('/comment', commentRouter)
-app.use('/poll', pollRouter)
-app.use('/user', userRouter)
-app.use('/vote', voteRouter)
+  app.use('/auth', authRouter)
+  app.use('/comment', commentRouter)
+  app.use('/poll', pollRouter)
+  app.use('/user', userRouter)
+  app.use('/vote', voteRouter)
 
-// Route client
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
-  app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/../../client/build/index.html'))
-  })
-} else {
-  app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/../../client/tmp/dev-server/index.html'))
+  // Route client
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.get('*', (req, res, next) => {
+      res.sendFile(path.join(__dirname + '/../../client/build/index.html'))
+    })
+  } else {
+    app.get('*', (req, res, next) => {
+      res.sendFile(path.join(__dirname + '/../../client/tmp/dev-server/index.html'))
+    })
+  }
+
+  app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
   })
 }
-
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
-})
+init()
